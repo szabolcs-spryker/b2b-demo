5b4a51860ae4ba3632e574d62ed7a50627ce6389
Szabolcs <szabolcs.dotnet@gmail.com>
2023-10-18 13:07:28 +0200

    Add DetailsFormDataProviderPlugin

---
+++ src/Pyz/Yves/CheckoutPage/Plugin/DetailsFormDataProviderPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Yves\CheckoutPage\Plugin;
+
+use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
+use Spryker\Yves\Kernel\AbstractPlugin;
+use Spryker\Yves\StepEngine\Dependency\Form\StepEngineFormDataProviderInterface;
+
+/**
+ * @method \SprykerShop\Yves\CheckoutPage\CheckoutPageFactory getFactory()
+ */
+class DetailsFormDataProviderPlugin extends AbstractPlugin implements StepEngineFormDataProviderInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Spryker\Shared\Kernel\Transfer\AbstractTransfer
+     */
+    public function getData(AbstractTransfer $quoteTransfer)
+    {
+        return $this->getFactory()->createDetailsDataProvider()->getData($quoteTransfer);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return array<string, mixed>
+     */
+    public function getOptions(AbstractTransfer $quoteTransfer)
+    {
+        return $this->getFactory()->createDetailsDataProvider()->getOptions($quoteTransfer);
+    }
+}
