f616e7cbbb1ee4ea991f9aba2758b09cfd26a22a
Szabolcs <szabolcs.dotnet@gmail.com>
2023-10-18 13:09:49 +0200

    Add DI for DetailsForm

--- src/Pyz/Yves/CheckoutPage/CheckoutPageFactory.php
+++ src/Pyz/Yves/CheckoutPage/CheckoutPageFactory.php
@@ -8,6 +8,8 @@
 namespace Pyz\Yves\CheckoutPage;
 
 use Pyz\Yves\CheckoutPage\Form\FormFactory;
+use Pyz\Yves\CheckoutPage\Form\Steps\DetailsForm;
+use Pyz\Yves\CheckoutPage\CheckoutPageDependencyProvider;
 use SprykerShop\Yves\CheckoutPage\CheckoutPageFactory as SprykerCheckoutPageFactory;
 
 class CheckoutPageFactory extends SprykerCheckoutPageFactory
@@ -18,5 +20,39 @@ class CheckoutPageFactory extends SprykerCheckoutPageFactory
     public function createPyzCheckoutFormFactory(): FormFactory
     {
         return new FormFactory();
+    }
+
+    /**
+     * @return \Spryker\Yves\StepEngine\Form\FormCollectionHandlerInterface
+     */
+    public function createDetailsFormCollection()
+    {
+        return $this->createFormCollection($this->getDetailsFormTypes(), $this->getDetailsFormDataProviderPlugin());
+    }
+
+    /**
+     * @return \Spryker\Yves\StepEngine\Dependency\Form\StepEngineFormDataProviderInterface
+     */
+    public function getDetailsFormDataProviderPlugin()
+    {
+        return $this->getProvidedDependency(CheckoutPageDependencyProvider::PLUGIN_DETAILS_FORM_DATA_PROVIDER);
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function getDetailsFormTypes()
+    {
+        return [
+            $this->getDetailsForm(),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getDetailsForm()
+    {
+        return DetailsForm::class;
     }
 }
